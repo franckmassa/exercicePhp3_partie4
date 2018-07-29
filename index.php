9a440ecd6923e7696e991ff5a801bbe10b8607b8
massa <massafranck@gmail.com>
2018-07-29 21:57:49 +0200

    initial

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice3</title>
+  </head>
+  <body>
+    <?php
+    //Déclaration de la fonction avec comme paramètres les deux chaines de caractères
+    function doubleChain($chain1, $chain2){
+    //La fonction retourne la concaténation des deux chaines de caractères
+      return  $chain1.$chain2;
+    }
+    //On affiche les deux chaines de caractères
+    echo doubleChain('Première chaine ', 'Deuxième chaine ');
+     ?>
+  </body>
+</html>
