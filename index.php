4619c2d2fd495cfe1af99ea8f33194f1a4ea23ef
massa <massafranck@gmail.com>
2018-08-01 21:46:21 +0200

    corrigé

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8" />
-    <title>Exercice3</title>
-  </head>
-  <body>
-    <p>
-      <?php
-      //Déclaration de la fonction avec comme paramètres les deux chaines de caractères
-      function doubleChain($chain1, $chain2){
-        //La fonction retourne la concaténation des deux chaines de caractères
-        return  $chain1.$chain2;
-      }
-      //On affiche les deux chaines de caractères
-      echo doubleChain('Première chaine ', 'Deuxième chaine ');
-      ?>
-    </p>
-  </body>
+<head>
+  <meta charset="utf-8" />
+  <title>exercice3</title>
+</head>
+<body>
+  <p>
+  <?php
+  // création de la fonction returnSring
+  function returnString($firstString, $secondString) {
+    return $firstString . $secondString;
+  }
+  // affichage de la fonction
+  echo returnString('Bon','jour');
+  ?>
+ </p>
+</body>
 </html>
